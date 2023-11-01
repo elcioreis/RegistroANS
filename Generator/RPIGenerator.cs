1b3676e6056797d08c61a12becaab0311a7cbaeb
Elcio Reis <elcio.reis@gmail.com>
2023-11-01 17:32:31 -0300

    Ajustes no tratamento de linhas do XLS

--- Generator/RPIGenerator.cs
+++ Generator/RPIGenerator.cs
@@ -15,6 +15,9 @@ internal class RPIGenerator
             Console.WriteLine($"Carregando o arquivo {origin}");
             try
             {
+                int exportadas = 0;
+                int ignoradas = 0;
+
                 var xls = new XLWorkbook(origin);
                 var planilha = xls.Worksheets.First();
 
@@ -29,6 +32,7 @@ internal class RPIGenerator
                 Console.WriteLine($"Total de {totalLinhas - 1} linhas no arquivo");
 
                 var operadora = new RPI_Operadora();
+
                 operadora.RegistroANS = planilha.Cell("A2").Value.ToString();
                 operadora.CnpjOperadora = planilha.Cell("B2").Value.ToString();
 
@@ -38,25 +42,37 @@ internal class RPIGenerator
 
                 for (var i = 2; i <= totalLinhas; i++)
                 {
-                    var inclusao = new RPI_InclusaoPrestador();
+                    //string status = planilha.Cell($"T{i}").Value.ToString().ToLower();
+                    //var cell = planilha.Cell($"T{i}").Value;
+                    var row = planilha.Row(i);
 
-                    inclusao.Classificacao = planilha.Cell($"E{i}").Value.ToString();
-                    inclusao.CnpjCpf = planilha.Cell($"F{i}").Value.ToString();
-                    inclusao.Cnes = planilha.Cell($"G{i}").Value.ToString();
-                    inclusao.Uf = planilha.Cell($"H{i}").Value.ToString();
-                    inclusao.CodigoMunicipioIBGE = planilha.Cell($"I{i}").Value.ToString().Substring(0, 6);
-                    inclusao.RazaoSocial = planilha.Cell($"J{i}").Value.ToString();
-                    inclusao.RelacaoOperadora = planilha.Cell($"K{i}").Value.ToString();
-                    inclusao.TipoContratualizacao = planilha.Cell($"L{i}").Value.ToString();
-                    inclusao.RegistroANSOperadoraIntermediaria = planilha.Cell($"M{i}").Value.ToString();
-                    inclusao.DataContratualizacao = planilha.Cell($"N{i}").Value.ToString();
-                    inclusao.DataInicioPrestacaoServico = planilha.Cell($"O{i}").Value.ToString();
-                    inclusao.DisponibilidadeServico = planilha.Cell($"P{i}").Value.ToString();
-                    inclusao.UrgenciaEmergencia = planilha.Cell($"Q{i}").Value.ToString();
-                    inclusao.Vinculacao.NumeroRegistroPlanoVinculacao = planilha.Cell($"R{i}").Value.ToString();
-                    inclusao.Vinculacao.CodigoPlanoOperadoraVinculacao = planilha.Cell($"S{i}").Value.ToString();
+                    if (!row.IsHidden)
+                    {
 
-                    solicitacao.InclusaoPrestador.Add(inclusao);
+                        var inclusao = new RPI_InclusaoPrestador();
+
+                        inclusao.Classificacao = planilha.Cell($"E{i}").Value.ToString();
+                        inclusao.CnpjCpf = planilha.Cell($"F{i}").Value.ToString();
+                        inclusao.Cnes = planilha.Cell($"G{i}").Value.ToString().PadLeft(7, '0');
+                        inclusao.Uf = planilha.Cell($"H{i}").Value.ToString();
+                        inclusao.CodigoMunicipioIBGE = planilha.Cell($"I{i}").Value.ToString().Substring(0, 6);
+                        inclusao.RazaoSocial = planilha.Cell($"J{i}").Value.ToString();
+                        inclusao.RelacaoOperadora = planilha.Cell($"K{i}").Value.ToString();
+                        inclusao.TipoContratualizacao = planilha.Cell($"L{i}").Value.ToString();
+                        inclusao.RegistroANSOperadoraIntermediaria = planilha.Cell($"M{i}").Value.ToString();
+                        inclusao.DataContratualizacao = planilha.Cell($"N{i}").Value.ToString();
+                        inclusao.DataInicioPrestacaoServico = planilha.Cell($"O{i}").Value.ToString();
+                        inclusao.DisponibilidadeServico = planilha.Cell($"P{i}").Value.ToString();
+                        inclusao.UrgenciaEmergencia = planilha.Cell($"Q{i}").Value.ToString();
+                        inclusao.Vinculacao.NumeroRegistroPlanoVinculacao = planilha.Cell($"R{i}").Value.ToString();
+                        inclusao.Vinculacao.CodigoPlanoOperadoraVinculacao = planilha.Cell($"S{i}").Value.ToString();
+                        solicitacao.InclusaoPrestador.Add(inclusao);
+                        exportadas++;
+                    }
+                    else
+                    {
+                        ignoradas++;
+                    }
                 }
 
                 operadora.solicitacao = solicitacao;
@@ -68,7 +84,7 @@ internal class RPIGenerator
 
                 if (string.IsNullOrEmpty(destination))
                 {
-                    destination = diretorio + "\\" + "RPI_" + operadora.RegistroANS + "_" + DateTime.Now.ToString("yyMMddHHmm") + ".rpi";
+                    destination = diretorio + "\\" + "C" + operadora.RegistroANS + "_" + DateTime.Now.ToString("yyMMddHHmm") + ".rpi";
                 }
 
                 using (var sw = new Utf8StringWriter())
@@ -90,6 +106,10 @@ internal class RPIGenerator
                 }
 
                 Console.WriteLine($"Arquivo {destination} foi gerado");
+
+                Console.WriteLine($"Total de {exportadas} linhas exportada");
+                Console.WriteLine($"Total de {ignoradas} linhas ignoradas");
+
             }
             catch (IOException ex)
             {
