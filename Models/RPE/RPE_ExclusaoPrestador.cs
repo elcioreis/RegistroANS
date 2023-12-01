b96ac3372f0b8616661418c33c4516d0e7762492
Elcio Reis <elcio.reis@gmail.com>
2023-12-01 11:52:40 -0300

    Classes RPE

---
+++ Models/RPE/RPE_ExclusaoPrestador.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace RegistroANS.Models.RPE;
+
+[XmlType(TypeName = "InclusaoPrestador")]
+public class RPE_ExclusaoPrestador
+{
+    [XmlElement(ElementName = "cnpjCpf")]
+    public string CnpjCpf { get; set; } = null!;
+
+    [XmlElement(ElementName = "cnes")]
+    public string Cnes { get; set; } = null!;
+
+    [XmlElement(ElementName = "codigoMunicipioIBGE")]
+    public string CodigoMunicipioIBGE { get; set; } = null!;
+}
---
+++ Models/RPE/RPE_Solicitacao.cs
@@ -0,0 +1,14 @@
+﻿using RegistroANS.Models.RPE;
+using System.Xml.Serialization;
+
+namespace RegistroANS.Models.RPI;
+public class RPE_Solicitacao
+{
+    public RPE_Solicitacao()
+    {
+        ExclusaoPrestador = new List<RPE_ExclusaoPrestador>();
+    }
+
+    [XmlElement(ElementName = "inclusaoPrestador", Type = typeof(RPE_ExclusaoPrestador))]
+    public List<RPE_ExclusaoPrestador> ExclusaoPrestador { get; set; } = null!;
+}
